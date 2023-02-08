057b95e59259f21d39b5f381c60e15af8305f1de
Асылбекович11 <bekturovbektur718@gmail.com>
2023-02-08 20:53:13 +0600

    first-commit

---
+++ src/hooks/useTitle.jsx
@@ -0,0 +1,12 @@
+import { useCallback } from "react"
+
+
+export const useTitle = () => {
+    const rename = useCallback((value) => {
+        document.title = value
+    }, [])
+
+    return {
+        rename
+    }
+}
